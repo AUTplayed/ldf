5e50d5e8bb8ff04c1f2b1d228968adeb5d869ee3
AUTplayed <fips.hem@gmail.com>
2018-06-15 16:17:54 +0200

    now waiting on external resources

--- ldf.js
+++ ldf.js
@@ -1,4 +1,7 @@
 var ldf = {
+	notfound: "<div>Page not Found</div>",
+	waitselector: "link[rel='stylesheet'],:not(script)[src]",
+	pagedir: "/pages",
 	nav: function (location) {
 		history.pushState(null, "", location);
 		ldf.locchange();
@@ -10,24 +13,32 @@ var ldf = {
 		ldf.load(loc);
 	},
 	load: function (loc) {
-		loc = "/pages" + loc + loc + ".html";
+		loc = ldf.pagedir + loc + loc + ".html";
 		ldf.helpers.request(loc, function (succ, content) {
-			if (succ) {
+			if (succ && ldf.notfound !== undefined) {
 				ldf.change(content);
 			} else {
-				ldf.change("<div>Page not Found</div>");
+				ldf.change(ldf.notfound);
 			}
 		});
 	},
 	change: function (content) {
 		var ldfnode = document.querySelector("#ldf");
-		ldfnode.innerHTML = content;
+		var ldfclone = ldfnode.cloneNode();
+		ldfclone.innerHTML = content;
+		ldfclone.style.display = "none";
+		ldfnode.parentElement.insertBefore(ldfclone, ldfnode);
+		ldf.helpers.waitForLoad(ldfclone.querySelectorAll(ldf.waitselector),
+			function () {
+				ldf.helpers.switch(ldfnode, ldfclone);
+			}
+		);
 		ldf.updatePageLinks();
-		ldf.loadScripts(ldfnode);
+		ldf.loadScripts(ldfclone);
 	},
 	updatePageLinks: function () {
 		document.querySelectorAll("a").forEach(function (a) {
-			a.removeEventListener("click", ldf.helpers.listener)
+			a.removeEventListener("click", ldf.helpers.listener);
 			a.addEventListener("click", ldf.helpers.listener);
 		});
 	},
@@ -42,6 +53,25 @@ var ldf = {
 		});
 	},
 	helpers: {
+		waitForLoad: function (loadings, cb) {
+			if (loadings.length > 0) {
+				var loadedcounter = 0;
+				loadings.forEach(function (loading) {
+					loading.onload = function () {
+						loadedcounter++;
+						if (loadedcounter == loadings.length) {
+							cb();
+						}
+					}
+				});
+			} else {
+				cb();
+			}
+		},
+		switch: function (og, clone) {
+			og.remove();
+			clone.style.display = "";
+		},
 		listener: function (e) {
 			e.preventDefault();
 			ldf.nav(e.target.getAttribute("href"));
@@ -54,7 +84,7 @@ var ldf = {
 				if (req.readyState === 4) {
 					cb(req.status.toString().startsWith("2"), req.response);
 				}
-			}
+			};
 		}
 	}
 };
